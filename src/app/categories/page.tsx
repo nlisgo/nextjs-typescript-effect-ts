ea5e96ee6090867124954a23b4e660be3be93ecf
nlisgo <n.lisgo@elifesciences.org>
2025-11-18 12:53:55 +0000

    Simplify CategoriesPage with Effect.promise for cleaner async handling

--- src/app/categories/page.tsx
+++ src/app/categories/page.tsx
@@ -10,15 +10,13 @@ export const metadata: Metadata = {
   title: 'Categories',
 };
 
-const CategoriesPage = async (): Promise<JSX.Element> => <Page>
-  {
-    Effect.runPromise(getCategories({ imageWidth: 80, imageHeight: 80 }).pipe(
-      Effect.map(Either.fromNullable(() => new Error('no categories found'))),
-      Effect.flatMap(Either.map((cats) => <section key="categories"><Categories title="Categories" categories={[...cats]} /></section>)),
-    ).pipe(
-      Effect.provide(FetchHttpClient.layer),
-    ))
-  }
-</Page>;
+const CategoriesPage = async (): Promise<JSX.Element> => Effect.runPromise(
+  getCategories({ imageWidth: 80, imageHeight: 80 }).pipe(
+    Effect.map(Either.fromNullable(() => new Error('no categories found'))),
+    Effect.flatMap(Either.map((cats) => <Page><section key="categories"><Categories title="Categories" categories={[...cats]} /></section></Page>)),
+  ).pipe(
+    Effect.provide(FetchHttpClient.layer),
+  ),
+);
 
 export default CategoriesPage;
