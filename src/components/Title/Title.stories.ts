e3df3c93d364213dd60acc7eac93077189eafbac
nlisgo <n.lisgo@elifesciences.org>
2025-11-19 00:04:34 +0000

    Add Title component stories to showcase basic and complex usage

---
+++ src/components/Title/Title.stories.ts
@@ -0,0 +1,39 @@
+import type { Meta, StoryObj } from '@storybook/nextjs-vite';
+import { Title } from '@/components/Title/Title';
+
+const meta = {
+  title: 'Example/Title',
+  component: Title,
+  tags: ['autodocs'],
+} satisfies Meta<typeof Title>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    content: 'Basic title',
+  },
+};
+
+export const Complex: Story = {
+  args: {
+    content: [
+      'This ',
+      {
+        type: 'Emphasis',
+        content: [
+          'is',
+        ],
+      },
+      ' a ',
+      {
+        type: 'Subscript',
+        content: [
+          'more',
+        ],
+      },
+      ' of a complex title',
+    ],
+  },
+};
