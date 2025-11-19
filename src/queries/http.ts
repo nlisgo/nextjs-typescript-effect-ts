3be56ddf7cbb0901f7eec7c603924a58703abbd9
nlisgo <n.lisgo@elifesciences.org>
2025-11-19 16:48:27 +0000

    revert: remove Effect Cache implementation
    Reverting to Map-based cache to prepare for persistent JSON cache feature.
    The persistent cache will support saving/loading API responses as JSON fixtures.

--- src/queries/http.ts
+++ src/queries/http.ts
@@ -1,24 +1,27 @@
 import { HttpClient } from '@effect/platform';
-import {
-  Cache, Effect, pipe, Schema,
-} from 'effect';
+import { Effect, pipe, Schema } from 'effect';
 
 const httpRequestAndValidate = <Resp, E1, R1, Body, E2, R2>(
   request: (uri: string) => Effect.Effect<Resp, E1, R1>,
   extract: (resp: Resp) => Effect.Effect<Body, E2, R2>,
 ) => <A, I = unknown, Req = never>(schema: Schema.Schema<A, I, Req>) => {
-  const makeCache = Cache.make({
-    capacity: 100,
-    timeToLive: Infinity,
-    lookup: (uri: string) => pipe(
+  const cache = new Map<string, A>();
+
+  return (uri: string) => Effect.suspend(() => {
+    if (cache.has(uri)) {
+      return Effect.succeed(cache.get(uri)!);
+    }
+
+    return pipe(
       uri,
       request,
       Effect.flatMap(extract),
       Effect.flatMap(Schema.decodeUnknown(schema)),
-    ),
+      Effect.tap((value) => Effect.sync(() => {
+        cache.set(uri, value);
+      })),
+    );
   });
-
-  return (uri: string) => Effect.flatMap(makeCache, (cache) => cache.get(uri));
 };
 
 export const httpGetAndValidate = httpRequestAndValidate(
